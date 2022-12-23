6eb7c02352b67e9e71ec311dc88f6a6aeaa306c9
Voloshina <sokolovskaya@anna.ru>
2022-12-23 23:55:11 +0300

    Конструктор класса

--- app/src/main/java/com/example/sokolovskayaanna/MainActivity.java
+++ app/src/main/java/com/example/sokolovskayaanna/MainActivity.java
@@ -6,9 +6,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Cat myCat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myCat = new Cat(4,"Puma","Bars","Yellow");
+        myCat.talk();
+
+        Cat murzik = new Cat(2,"Murzik","Murz","White");
+        murzik.talk();
+
+        Cat glasha = new Cat();
+        glasha.age = 1;
+        glasha.name = "Glsha";
     }
 }
