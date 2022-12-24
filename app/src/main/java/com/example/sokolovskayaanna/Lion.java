f37b1b9edbae2299ee0299b63f55ce1591a4a840
Voloshina <sokolovskaya@anna.ru>
2022-12-24 15:01:41 +0300

    Статические внутренние классы

--- app/src/main/java/com/example/sokolovskayaanna/Lion.java
+++ app/src/main/java/com/example/sokolovskayaanna/Lion.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class Lion extends Cat {
     public void talk() {
-        Log.i("talk()", "R-r-r! I'm lion. My name is " + name + ", and I'm " + age + " yaers old." + Lion.whatCatsLike() + "My breed " + breed + ", my color is " + color);
+        Log.i("talk()", "R-r-r! I'm lion. My name is " + name + ", and I'm " + age + " yaers old." + Lion.whatCatsLike());
     }
 
     public static String whatCatsLike() {
--- app/src/main/java/com/example/sokolovskayaanna/Puma.java
+++ app/src/main/java/com/example/sokolovskayaanna/Puma.java
@@ -5,6 +5,6 @@ import android.util.Log;
 
 public class Puma extends Cat {
     public void talk() {
-        Log.i("talk()", "R-r-r! I'm puma.My name is " + name + ", and I'm " + age + " yaers old. My breed " + breed + ", my color is " + color);
+        Log.i("talk()", "R-r-r! I'm puma.My name is " + name + ", and I'm " + age + " yaers old.");
     }
 }
